80cc86330cea32bc8a19b8831414681ce77c4772
dharuhjan <dharuhjans@gmail.com>
2024-02-17 09:20:47 +0530

    Updated README.md with new information

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '916dbc2570mshf243dee2ecca9e5p1a087fjsnb00a9da65808',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -11,4 +11,4 @@ export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL= "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY="66d1af9aff08e9f6fc060bc65f6047d0";
+export const WEATHER_API_KEY="";
